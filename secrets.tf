194da358d581c2be5d5bc8bddf74f5eeca76c3a6
Datta Chaitanya Attili <dattaattili@gmail.com>
2019-06-03 18:36:23 -0400

    Update secrets.tf

--- secrets.tf
+++ secrets.tf
@@ -7,8 +7,8 @@ module "test_secrets" {
    kms_key_name                = "test"
    name                        = "ss"
    key_values                  = {
-    username                   = "value1"
-    password                   = "value2"
+    input1                   = "value1"
+    input2                   = "value2"
     port                       = "value3"
   }
 }
